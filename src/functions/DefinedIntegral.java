9d723b484bbea6eee0efbb63a50f60777dfd87ca
Deidarik <113616148+Deidarik@users.noreply.github.com>
2023-10-01 16:01:51 +0400

    добавка

---
+++ src/functions/DefinedIntegral.java
@@ -0,0 +1,18 @@
+package functions;
+import java.util.Scanner;
+public class DefinedIntegral implements MathFunction {
+    double x;
+    public String func;
+    DefinedIntegral()
+    {
+        Scanner in = new Scanner(System.in);
+        func = in.nextLine();
+    }
+    public static double apply(double x, String f)
+    {
+        double s =0;
+        char b;
+
+        return s;
+    }
+}
