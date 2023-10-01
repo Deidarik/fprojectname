6d2b2f62210a3a33a1d5c424df92cc0d15101b46
Валеев <tastyjam4human@gmail.com>
2023-10-01 19:17:14 +0400

    Added comments

--- Lab_2/src/main/java/functions/SqrFunction.java
+++ Lab_2/src/main/java/functions/SqrFunction.java
@@ -4,8 +4,5 @@ import static java.lang.Math.pow;
 
 public class SqrFunction implements MathFunction {
     double x;
-    public static double apply(double x)
-    {
-        return pow(x,2);
-    }
+    public static double apply(double x) {return pow(x, 2);}   //Возводим в квадрат
 }
