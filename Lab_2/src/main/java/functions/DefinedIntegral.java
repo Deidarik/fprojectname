5095e53449d994ab098b4038a1c9ac430615b113
Валеев <tastyjam4human@gmail.com>
2023-10-04 02:18:35 +0400

    Not completely done Integral class.

--- Lab_2/src/main/java/functions/DefinedIntegral.java
+++ Lab_2/src/main/java/functions/DefinedIntegral.java
@@ -1,64 +1,122 @@
 package functions;
+import java.util.ArrayList;
 
-import java.util.*;
-
-public class DefinedIntegral implements MathFunction {
-    double x;
-    public static String func;
-
-    DefinedIntegral() {
-        Scanner in = new Scanner(System.in);
-        func = in.nextLine();
-    }
-
-    public enum Conditions
-    {
-        S0,S1,S2,S3,S4,S5,S6;
-    }
-
-    public enum Signals{
-        Funlet, Numb,X,Ls,Lr,xP
-    }
-    public void createtable( ){
-
-    }
-    public static double FindIntegral(String tmpa, String tmpf)
-    {
-        double r=0;
-        List<String> TempA = new LinkedList<String>();
-        for(int j=0;j<tmpa.length();++j)
+class Analyze
+{
+    enum TypesOfWords{
+        start(0),space(1),numbr(2),word_(3),openBr(4),closeBr(5),oper(6),error(7);
+        TypesOfWords(int argument)
         {
+            this.argument = argument;
+        }
+        final private int argument;
+        public int getArgument(){return argument;}
+    }
+    static class PartsOfText
+    {
+        public String word;
+        public TypesOfWords type;
+        PartsOfText(String s, TypesOfWords t)
+        {
+            word = s;
+            type = t;
+        }
+    }
+    private final String sentence ;
+    public ArrayList<PartsOfText> readyParts;
+    Analyze(String l)
+    {
+        sentence = l;
+        readyParts = new ArrayList<>();
+    }
+    private boolean isDigit(char c)
+    {
+        return (('1' <= c)&&(c<='9'));
+    }
+    private boolean isLetter(char c)
+    {
+        return (('a' <= c) && (c<='z'));
+     }
+    private boolean isArithOper(char c)
+    {
+        return (c == '+' || c == '-' || c == '/' || c == '*');
+    }
+    private boolean isSpace(char c)
+    {
+        return (c == ' ');
+    }
+    private TypesOfWords typeOfChar(char c)
+    {
+        if(isLetter(c))
+            return TypesOfWords.word_;
+        else if(isDigit(c))
+            return TypesOfWords.numbr;
+        else if(isArithOper(c))
+            return TypesOfWords.oper;
+        else if(isSpace(c))
+            return TypesOfWords.space;
+        else if(c == '(')
+            return TypesOfWords.openBr;
+        else if(c == ')')
+            return TypesOfWords.closeBr;
+        else
+            return TypesOfWords.error;
+    }
+    public void firstAnalyze()
+    {
+        char curChar;
+        String word = "";
+        TypesOfWords prevState ;
+        TypesOfWords state = TypesOfWords.start;
+        TypesOfWords[][] table = new TypesOfWords[][]
+                {
+                //    start                space             number              word                openBracket         closeBracket         oper                error
+                {TypesOfWords.error,  TypesOfWords.error,  TypesOfWords.error,  TypesOfWords.error,  TypesOfWords.error, TypesOfWords.error,  TypesOfWords.error, TypesOfWords.error},// start
+                {TypesOfWords.space,  TypesOfWords.space,  TypesOfWords.space,  TypesOfWords.space,  TypesOfWords.space, TypesOfWords.space,  TypesOfWords.space, TypesOfWords.error},// space
+                {TypesOfWords.numbr,  TypesOfWords.numbr,  TypesOfWords.numbr,  TypesOfWords.error,  TypesOfWords.numbr, TypesOfWords.error,  TypesOfWords.numbr, TypesOfWords.error},// number
+                {TypesOfWords.word_,  TypesOfWords.word_,  TypesOfWords.error,  TypesOfWords.word_,  TypesOfWords.word_, TypesOfWords.error,  TypesOfWords.word_, TypesOfWords.error},// word
+                {TypesOfWords.openBr, TypesOfWords.openBr, TypesOfWords.error,  TypesOfWords.openBr, TypesOfWords.openBr,TypesOfWords.error,  TypesOfWords.openBr,TypesOfWords.error},// openBracket
+                {TypesOfWords.closeBr,TypesOfWords.closeBr,TypesOfWords.closeBr,TypesOfWords.closeBr,TypesOfWords.error, TypesOfWords.closeBr,TypesOfWords.error, TypesOfWords.error},// closeBracket
+                {TypesOfWords.error,  TypesOfWords.oper,   TypesOfWords.oper,   TypesOfWords.oper ,  TypesOfWords.error, TypesOfWords.oper,   TypesOfWords.error, TypesOfWords.error},// oper
+
+                };
+
+        for(int i =0; i < sentence.length(); ++i)
+        {
+            curChar = sentence.charAt(i);
+
+            prevState = state;
+            state = table[typeOfChar(curChar).getArgument()][state.getArgument()];
+
+            if(prevState != state && prevState != TypesOfWords.start)
+            {
+                if(prevState != TypesOfWords.space)
+                {
+                    PartsOfText ready;
+                    ready = new PartsOfText(word, prevState);
+
+
+                    readyParts.add(ready);
+                    word = "";
+
+                }
+
+            }
+            if(!isSpace(curChar))
+                word+=curChar ;
 
         }
-        return r;
-    }
-    public static double apply(double x, String f) {
-        List<Double> TempRes = new LinkedList<Double>();
-        List<Character> TempOp = new LinkedList<Character>();
-        String tmpf="";
-        String tmpa="";
-        boolean is_fun = true;
-        double s = 0;
-        for (int i = 0; i < func.length(); ++i) {
-            if (func.charAt(i)!=' ') {
-                if (func.charAt(i) == '+' || func.charAt(i)  == '-' || func.charAt(i) == '*') TempOp.add(func.charAt(i));
-                else {
-                    if (func.charAt(i) != '(' && func.charAt(i) != ')') {
-                        tmpf+=func.charAt(i);
-                    }
-                    else
-                    {
-                        is_fun = (func.charAt(i)== ')') ? true: false;
-                        tmpa+=func.charAt(i);
-                    }
-                }
-            }
-            else {
-                TempRes.add(FindIntegral(tmpa,tmpf));
-                tmpa="";
-                tmpf="";
-            }
+        if(!word.isEmpty())
+        {
+            PartsOfText ready = new PartsOfText(word, state);
+            readyParts.add(ready);
+            word = "";
         }
-        return s;
+
     }
 }
+//public class DefinedIntegral implements MathFunction {
+//
+//
+//
+//}
