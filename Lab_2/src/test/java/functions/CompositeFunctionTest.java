022790ea376447175e2d3ea3308908d668ce5ddd
Deidarik <113616148+Deidarik@users.noreply.github.com>
2023-10-07 15:53:43 +0400

    добавил классы наших функций, тесты по методу интерфейса MathFunction закинул в Composite

--- Lab_2/src/test/java/functions/CompositeFunctionTest.java
+++ Lab_2/src/test/java/functions/CompositeFunctionTest.java
@@ -12,12 +12,21 @@ class CompositeFunctionTest {
         SqrFunction sf = new SqrFunction();
         CompositeFunction cf = new CompositeFunction(iF,sf);
         CompositeFunction cf2 =  new CompositeFunction(sf,cf);
+        MathFunction test2 = new AcosPlusAsin();
+        MathFunction test = new cosDoubleArg();
+        MathFunction CosTwoA = test.andThen(test2);
+        MathFunction CosInSq = sf.andThen(test);
         double expected = 100;
         double actual = cf.apply(10);
         assertEquals(expected,actual);
         expected = 10000;
         actual = cf2.apply(10);
         assertEquals(expected,actual);
+        expected = -1;
+        actual = CosTwoA.apply(0);
+        expected = 1;
+        actual = CosInSq.apply(0);
+
 
     }
 }
