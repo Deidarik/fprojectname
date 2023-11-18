b68756b0cb7d0f3477e7a4f73146d1754f4d4e1a
Deidarik <113616148+Deidarik@users.noreply.github.com>
2023-11-18 23:28:18 +0400

    1y//пс, создай отдельно у себя две папочки input и output без файлов на уровне .idea,src

--- Lab_2/src/main/java/io/FunctionsIO.java
+++ Lab_2/src/main/java/io/FunctionsIO.java
@@ -1,11 +1,22 @@
 package io;
 
+import functions.Point;
 import functions.TabulatedFunction;
 
 import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
 
 public final class FunctionsIO {
-    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function){
+    private FunctionsIO() {
+        throw new UnsupportedOperationException();
+    }
 
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function){
+        PrintWriter printWriter = new PrintWriter(outputStream);
+        printWriter.println(function.getCount());
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        printWriter.flush();
     }
 }
