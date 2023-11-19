2918fc2d4d7891d1af007b88775ef87cd423ae25
Deidarik <113616148+Deidarik@users.noreply.github.com>
2023-11-19 17:45:42 +0400

    ArrayTabulatedFunctionSerialization еще не доделан

--- Lab_2/src/main/java/io/ArrayTabulatedFunctionSerialization.java
+++ Lab_2/src/main/java/io/ArrayTabulatedFunctionSerialization.java
@@ -2,38 +2,38 @@ package io;
 
 import functions.ArrayTabulatedFunction;
 import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
 import operations.TabulatedDifferentialOperator;
 
 import java.io.*;
 
 public class ArrayTabulatedFunctionSerialization {
     public static void main(String[] args) {
-        try {
-            try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("output/serialized array function.bin"))) {
-                double[] xValues = {1, 2, 3};
-                double[] yValues = {1.5, 2.5, 3.5};
+        try{
+            try(BufferedOutputStream fileOutputStream = new BufferedOutputStream(new FileOutputStream("output/serialized_array_functions.bin"))){
+                double[] xValue = {1, 2, 3, 4, 5};
+                double[] yValue = {5, 10, 15, 20, 25};
 
-                TabulatedDifferentialOperator tabulatedDifferentialOperator = new TabulatedDifferentialOperator();
+                TabulatedFunction func = new ArrayTabulatedFunction(xValue,yValue);
+                TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+                TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator(factory);
 
-                TabulatedFunction arrayFunc = new ArrayTabulatedFunction(xValues, yValues);
-                TabulatedFunction differenArrayFunc1 = tabulatedDifferentialOperator.derive(arrayFunc);
-                TabulatedFunction differenArrayFunc2 = tabulatedDifferentialOperator.derive(differenArrayFunc1);
+                TabulatedFunction derivativeFirst = operator.derive(func);
+                TabulatedFunction derivativeSecond = operator.derive(derivativeFirst);
 
-                FunctionsIO.serialize(bufferedOutputStream, arrayFunc);
-                FunctionsIO.serialize(bufferedOutputStream, differenArrayFunc1);
-                FunctionsIO.serialize(bufferedOutputStream, differenArrayFunc2);
-
+                FunctionsIO.serialize(fileOutputStream, func);
+                FunctionsIO.serialize(fileOutputStream, derivativeFirst);
+                FunctionsIO.serialize(fileOutputStream, derivativeSecond);
             }
-
-        } catch (IOException e) {
+        }catch (IOException e){
             e.printStackTrace();
         }
-        try {
-            try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("input/serialized array function.bin"))) {
-                System.out.println(FunctionsIO.deserialize(bufferedInputStream));
-                System.out.println(FunctionsIO.deserialize(bufferedInputStream));
-                System.out.println(FunctionsIO.deserialize(bufferedInputStream));
-            }
+        try (BufferedInputStream input = new BufferedInputStream(new FileInputStream("output/serialized_array_functions.bin"))) {
+            System.out.println(FunctionsIO.deserialize(input));
+            System.out.println(FunctionsIO.deserialize(input));
+            System.out.println(FunctionsIO.deserialize(input));
+
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
